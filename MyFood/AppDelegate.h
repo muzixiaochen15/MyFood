48216f1b151712a0a640059983d57053b4bd7bf4
@李群 <@muzixiaochen@163.com>
2016-06-17 09:18:29 +0800

    Initial Commit

---
+++ MyFood/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFood
+//
+//  Created by qunlee on 16/6/17.
+//  Copyright © 2016年 qunlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFood/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFood
+//
+//  Created by qunlee on 16/6/17.
+//  Copyright © 2016年 qunlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
